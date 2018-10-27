5ef86b6248ce35b892e73c29bc67d7bd79418689
Kyoji Osada <39667755+KyojiOsada@users.noreply.github.com>
2018-10-28 00:37:43 +0900

    Create RdbDumpConfig.php

---
+++ RdbDumpConfig.php
@@ -0,0 +1,13 @@
+<?php
+
+class RdbDumpConfig
+{
+	# Mail RCPT TO
+	public static $rcpt_tos = array(
+		'user1@example.com',
+		'user2@example.com',
+		'user3@example.com',
+		'user4@example.com',
+	);
+
+}
